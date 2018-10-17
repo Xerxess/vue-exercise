8dd4fbcdc5518d3f05a98dbe98bf7f2c3680d761
969371501@qq.com <969371501@qq.com>
2018-10-17 23:05:59 +0800

    提交

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,6 +1,16 @@
 <template>
 <div>
     <my-dialog></my-dialog>
+    <my-slot :data="{name:'小明'}">
+        <h1>slotContent</h1>
+        <template slot="header">
+    <h1>Here might be a page title</h1>
+  </template>
+  <p slot="content" class="a">slotcontent{{slotss}}</p>
+  <template slot="scope" slot-scope="myscope"> 
+     <span>{{myscope.data.name}}</span> 
+  </template>
+    </my-slot>
 </div>
 </template>
 
@@ -12,14 +22,26 @@ import DemoData from "./components/Demo.vue";
 import MyDialog from "./components/Dialog.vue";
 import layoutLess from './less/layout.less';
 import css from './css-modules/modules.css';
+import mySlot from './components/slot.vue';
 
 console.log(css);
 
 console.log(layoutLess);
 
 export default {
+    data:function(){
+        return {
+            slotss:'myslot'
+        }
+    },
+    methods:{
+        log:function(...arg){
+            console.log(arg);
+        }
+    },
     components: {
-        MyDialog
+        MyDialog,
+        mySlot
     }
 };
 </script>
---
+++ demo/src/components/slot.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+<slot></slot>
+<slot name="header"></slot>
+<slot name="content"></slot>
+ {{data.name}}
+<slot name="scope" :data="data"></slot>
+    </div> 
+</template>
+
+<script>
+export default {
+    data:function(){return {};},
+    props:['data']
+};
+</script>
+
+<style scoped>
+</style>
