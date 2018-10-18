5d416ee35b17fd345cce67627c79cd5c1ac80f45
unknown <969371501@qq.com>
2018-10-18 17:35:29 +0800

    render - jsx

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,107 +1,144 @@
-<template>
-<div>
-    <my-dialog></my-dialog>
-    <my-slot :data="{name:'小明'}">
-        <h1>slotContent</h1>
-        <template slot="header">
-    <h1>Here might be a page title</h1>
-  </template>
-  <p slot="content" class="a">slotcontent{{slotss}}</p>
-  <template slot="scope" slot-scope="myscope"> 
-     <span>{{myscope.data.name}}</span> 
-  </template>
-    </my-slot>
-</div>
-</template>
-
-
-
-      
-
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import DemoData from "./components/Demo.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+// import DemoData from "./components/Demo.vue";
 import MyDialog from "./components/Dialog.vue";
-import layoutLess from './less/layout.less';
-import css from './css-modules/modules.css';
-import mySlot from './components/slot.vue';
+// import layoutLess from "./less/layout.less";
+// import css from "./css-modules/modules.css";
+// import mySlot from "./components/slot.vue";
 
-console.log(css);
+// console.log(css);
 
-console.log(layoutLess);
+// console.log(layoutLess);
 
 export default {
-    data: function(){
-        return {
-            message: 'Vue的生命周期'
-        };
-    },
-    template:'<div class="myclass"><MyDialog></MyDialog>abc</div>',
+  data: function() {
+    return {
+      message: "Vue的生命周期"
+    };
+  },
+  components: {
+    MyDialog
+  },
+  render: function(h) {
+    const data = {
+      attrs: {
+        id: "hehe"
+      },
+      props: {
+        mytitle: "hihihi"
+      }
+    };
+    const props = {
+      mytitle: "title",
+      mycontent: "content"
+    };
+    let len = 7;
 
-    components: {
-        MyDialog
-    },
-    // beforeCreate: function () {
-    //     console.group('------beforeCreate创建前状态------');
-    //     console.log("%c%s", "color:red", "el     : " + this.$el); //undefined
-    //     console.log("%c%s", "color:red", "data   : " + this.$data); //undefined 
-    //     console.log("%c%s", "color:red", "message: " + this.message)
-    // },
-    // created: function () {
-    //     console.group('------created创建完毕状态------');
-    //     console.log("%c%s", "color:red", "el     : " + this.$el); //undefined
-    //     console.log("%c%s", "color:red", "data   : " + this.$data); //已被初始化 
-    //     console.log("%c%s", "color:red", "message: " + this.message); //已被初始化
-    // },
-    // beforeMount: function () {
-    //     console.group('------beforeMount挂载前状态------');
-    //     console.log("%c%s", "color:red", "el     : " + (this.$el)); //已被初始化
-    //     console.log(this.$el);
-    //     console.log("%c%s", "color:red", "data   : " + this.$data); //已被初始化  
-    //     console.log("%c%s", "color:red", "message: " + this.message); //已被初始化  
-    // },
-    // mounted: function () {
-    //     let _that = this;
-    //     console.group('------mounted 挂载结束状态------');
-    //     console.log("%c%s", "color:red", "el     : " + this.$el); //已被初始化
-    //     console.log(this.$el);
-    //     console.log("%c%s", "color:red", "data   : " + this.$data); //已被初始化
-    //     console.log("%c%s", "color:red", "message: " + this.message); //已被初始化 
-    //     setTimeout(function () {
-    //         console.log(_that);
-    //         _that.message = "修改data";
-    //         console.log("%c%s", "color:red", "message: " + _that.message); //已被初始化 
-    //     }, 2000);
-    // },
-    // beforeUpdate: function () {
-    //     console.group('beforeUpdate 更新前状态===============》');
-    //     console.log("%c%s", "color:red", "el     : " + this.$el);
-    //     console.log(this.$el);
-    //     console.log("%c%s", "color:red", "data   : " + this.$data);
-    //     console.log("%c%s", "color:red", "message: " + this.message);
-    // },
-    // updated: function () {
-    //     console.group('updated 更新完成状态===============》');
-    //     console.log("%c%s", "color:red", "el     : " + this.$el);
-    //     console.log(this.$el);
-    //     console.log("%c%s", "color:red", "data   : " + this.$data);
-    //     console.log("%c%s", "color:red", "message: " + this.message);
-    // },
-    // beforeDestroy: function () {
-    //     console.group('beforeDestroy 销毁前状态===============》');
-    //     console.log("%c%s", "color:red", "el     : " + this.$el);
-    //     console.log(this.$el);
-    //     console.log("%c%s", "color:red", "data   : " + this.$data);
-    //     console.log("%c%s", "color:red", "message: " + this.message);
-    // },
-    // destroyed: function () {
-    //     console.group('destroyed 销毁完成状态===============》');
-    //     console.log("%c%s", "color:red", "el     : " + this.$el);
-    //     console.log(this.$el);
-    //     console.log("%c%s", "color:red", "data   : " + this.$data);
-    //     console.log("%c%s", "color:red", "message: " + this.message)
-    // },
-
+    var Forrender = function() {
+      let arry = [];
+      [1, 2, 3, 4, 5, 6].forEach(element => {
+        arry.push(
+          <div>
+            <MyDialog {...data} />
+            <MyDialog
+              // normal attributes or component props.
+              id="foo"
+              mytitle="abc"
+              // DOM properties are prefixed with `domProps`
+              // event listeners are prefixed with `on` or `nativeOn`
+              // other special top-level properties
+              class={{ foo: true, bar: false }}
+              style={{ color: "red", fontSize: "14px" }}
+              key="key"
+              ref="ref"
+              // assign the `ref` is used on elements/components with v-for
+              refInFor
+              slot="slot"
+            />
+          </div>
+        );
+      });
+      return arry;
+    };
+    let ifa = 1;
+        var iffn = function() {
+      if (ifa == 1) {
+        return <MyDialog mytitle="lastone" />;
+      }
+    };
+    return (
+      <div class="myclass" onClick={this.click}>
+        {Forrender()}
+        {iffn()}
+      </div>
+    );
+  },
+  methods: {
+    click() {
+      this.message = "我修改了值";
+      console.log(this.message);
+    }
+  }
+  // beforeCreate: function () {
+  //     console.group('------beforeCreate创建前状态------');
+  //     console.log("%c%s", "color:red", "el     : " + this.$el); //undefined
+  //     console.log("%c%s", "color:red", "data   : " + this.$data); //undefined
+  //     console.log("%c%s", "color:red", "message: " + this.message)
+  // },
+  // created: function () {
+  //     console.group('------created创建完毕状态------');
+  //     console.log("%c%s", "color:red", "el     : " + this.$el); //undefined
+  //     console.log("%c%s", "color:red", "data   : " + this.$data); //已被初始化
+  //     console.log("%c%s", "color:red", "message: " + this.message); //已被初始化
+  // },
+  // beforeMount: function () {
+  //     console.group('------beforeMount挂载前状态------');
+  //     console.log("%c%s", "color:red", "el     : " + (this.$el)); //已被初始化
+  //     console.log(this.$el);
+  //     console.log("%c%s", "color:red", "data   : " + this.$data); //已被初始化
+  //     console.log("%c%s", "color:red", "message: " + this.message); //已被初始化
+  // },
+  // mounted: function () {
+  //     let _that = this;
+  //     console.group('------mounted 挂载结束状态------');
+  //     console.log("%c%s", "color:red", "el     : " + this.$el); //已被初始化
+  //     console.log(this.$el);
+  //     console.log("%c%s", "color:red", "data   : " + this.$data); //已被初始化
+  //     console.log("%c%s", "color:red", "message: " + this.message); //已被初始化
+  //     setTimeout(function () {
+  //         console.log(_that);
+  //         _that.message = "修改data";
+  //         console.log("%c%s", "color:red", "message: " + _that.message); //已被初始化
+  //     }, 2000);
+  // },
+  // beforeUpdate: function () {
+  //     console.group('beforeUpdate 更新前状态===============》');
+  //     console.log("%c%s", "color:red", "el     : " + this.$el);
+  //     console.log(this.$el);
+  //     console.log("%c%s", "color:red", "data   : " + this.$data);
+  //     console.log("%c%s", "color:red", "message: " + this.message);
+  // },
+  // updated: function () {
+  //     console.group('updated 更新完成状态===============》');
+  //     console.log("%c%s", "color:red", "el     : " + this.$el);
+  //     console.log(this.$el);
+  //     console.log("%c%s", "color:red", "data   : " + this.$data);
+  //     console.log("%c%s", "color:red", "message: " + this.message);
+  // },
+  // beforeDestroy: function () {
+  //     console.group('beforeDestroy 销毁前状态===============》');
+  //     console.log("%c%s", "color:red", "el     : " + this.$el);
+  //     console.log(this.$el);
+  //     console.log("%c%s", "color:red", "data   : " + this.$data);
+  //     console.log("%c%s", "color:red", "message: " + this.message);
+  // },
+  // destroyed: function () {
+  //     console.group('destroyed 销毁完成状态===============》');
+  //     console.log("%c%s", "color:red", "el     : " + this.$el);
+  //     console.log(this.$el);
+  //     console.log("%c%s", "color:red", "data   : " + this.$data);
+  //     console.log("%c%s", "color:red", "message: " + this.message)
+  // },
 };
 </script>
 
@@ -109,10 +146,10 @@ export default {
 
 <style>
 .dd {
-    margin: 0;
+  margin: 0;
 }
 
 .ss {
-    margin: 1;
+  margin: 1;
 }
 </style>
--- demo/src/components/Dialog.vue
+++ demo/src/components/Dialog.vue
@@ -4,18 +4,18 @@
         <div class="mask"></div>
         <div class="bem-dialog bem-dialog_color-blue_shadow">
             <div class="bem-dialog__header bem-dialog__title_border bem-dialog__title_margin10">
-                <span class="bem-dialog__title">{{title||'默认标题'}}</span>
+                <span class="bem-dialog__title">{{mytitle||'默认标题'}}</span>
                 <div class="bem-dialog__closeed bem-dialog__closeed_right J_Closeed">
                     <i class="bem-icon icon-close"></i></div>
             </div>
-            <div class="bem-dialog__content">{{title||'默认内容'}}
+            <div class="bem-dialog__content">{{mycontent||'默认内容'}}
             </div>
             <div class="bem-dialog__footer">
-                <button @click="closeed" class="bem-btn bem-btn_color-blue_shadow">我知道了</button>
+<button @click="closeed" class="bem-btn bem-btn_color-blue_shadow">我知道了</button>
             </div>
         </div>
     </div>
-    <p ref="refInfo" style="top:100px;position:absolute;z-index:10000;font-size:36px;">hello</p>
+    <p ref="refInfo" style="top:100px;position:absolute;z-index:10000;font-size:36px;">{{mytitle}}</p>
     <transition name="fade" :duration="1000"
      v-on:before-enter="beforeEnter" 
      v-on:enter="enter"
@@ -28,135 +28,130 @@
 
 <script>
 let inBrowser = true;
-let raf = inBrowser ?
-    window.requestAnimationFrame ?
-    window.requestAnimationFrame.bind(window) :
-    setTimeout :
-    /* istanbul ignore next */
-    function (fn) {
-        return fn();
+let raf = inBrowser
+  ? window.requestAnimationFrame
+    ? window.requestAnimationFrame.bind(window)
+    : setTimeout
+  : /* istanbul ignore next */
+    function(fn) {
+      return fn();
     };
 
 function addClass(el, cls) {
-    /* istanbul ignore if */
-    if (!cls || !(cls = cls.trim())) {
-        return;
-    }
+  /* istanbul ignore if */
+  if (!cls || !(cls = cls.trim())) {
+    return;
+  }
 
-    /* istanbul ignore else */
-    if (el.classList) {
-        if (cls.indexOf(" ") > -1) {
-            cls.split(/\s+/).forEach(function (c) {
-                return el.classList.add(c);
-            });
-        } else {
-            el.classList.add(cls);
-        }
+  /* istanbul ignore else */
+  if (el.classList) {
+    if (cls.indexOf(" ") > -1) {
+      cls.split(/\s+/).forEach(function(c) {
+        return el.classList.add(c);
+      });
     } else {
-        var cur = " " + (el.getAttribute("class") || "") + " ";
-        if (cur.indexOf(" " + cls + " ") < 0) {
-            el.setAttribute("class", (cur + cls).trim());
-        }
+      el.classList.add(cls);
     }
+  } else {
+    var cur = " " + (el.getAttribute("class") || "") + " ";
+    if (cur.indexOf(" " + cls + " ") < 0) {
+      el.setAttribute("class", (cur + cls).trim());
+    }
+  }
 }
 
 function removeClass(el, cls) {
-    /* istanbul ignore if */
-    if (!cls || !(cls = cls.trim())) {
-        return;
-    }
+  /* istanbul ignore if */
+  if (!cls || !(cls = cls.trim())) {
+    return;
+  }
 
-    /* istanbul ignore else */
-    if (el.classList) {
-        if (cls.indexOf(" ") > -1) {
-            cls.split(/\s+/).forEach(function (c) {
-                return el.classList.remove(c);
-            });
-        } else {
-            el.classList.remove(cls);
-        }
-        if (!el.classList.length) {
-            el.removeAttribute("class");
-        }
+  /* istanbul ignore else */
+  if (el.classList) {
+    if (cls.indexOf(" ") > -1) {
+      cls.split(/\s+/).forEach(function(c) {
+        return el.classList.remove(c);
+      });
     } else {
-        var cur = " " + (el.getAttribute("class") || "") + " ";
-        var tar = " " + cls + " ";
-        while (cur.indexOf(tar) >= 0) {
-            cur = cur.replace(tar, " ");
-        }
-        cur = cur.trim();
-        if (cur) {
-            el.setAttribute("class", cur);
-        } else {
-            el.removeAttribute("class");
-        }
+      el.classList.remove(cls);
     }
+    if (!el.classList.length) {
+      el.removeAttribute("class");
+    }
+  } else {
+    var cur = " " + (el.getAttribute("class") || "") + " ";
+    var tar = " " + cls + " ";
+    while (cur.indexOf(tar) >= 0) {
+      cur = cur.replace(tar, " ");
+    }
+    cur = cur.trim();
+    if (cur) {
+      el.setAttribute("class", cur);
+    } else {
+      el.removeAttribute("class");
+    }
+  }
 }
 export default {
-    props: {
-        title: "String",
-        content: "String"
-    },
-    data: function () {
-        console.log(this);
-        return {
-            show: false
-        };
-    },
-    methods: {
-        closeed() {
-            let refs = this.$refs;
-            console.log(refs);
-            let el = refs.refInfo;
-            if (this.show) {
-                this.show = false;
-                 el.style.display = "none";
-                removeClass(el, "fade-enter");
-                removeClass(el, "fade-enter-to");
-                removeClass(el, "fade-enter-active");
-            } else {
-                this.show = true;
-                 el.style.display = "block";
-                addClass(el, "fade-enter");
-                addClass(el, "fade-enter-active");
-                raf(function () {
-                  raf(function () {
-                        removeClass(el, "fade-enter");
-                        addClass(el, "fade-enter-to");
-                        let onend = function () {
-                            console.log("执行完毕！");
-                            removeClass(el, "fade-enter-to");
-                            removeClass(el, "fade-enter-active");
-                            el.removeEventListener("transitionend", onend);
-                        }
-                        el.addEventListener("transitionend", onend);
-                    });
-                });
-                  
-               
-            }
+  props: {
+    mytitle: String,
+    mycontent: String
+  },
+  data: function() {
+    console.log(this);
+    return {
+      show: false
+    };
+  },
+  beforeMount() {
+    console.log(this);
+  },
+  methods: {
+    closeed() {
+      let refs = this.$refs;
+      console.log(refs);
+      let el = refs.refInfo;
+      if (this.show) {
+        this.show = false;
+        el.style.display = "none";
+        removeClass(el, "fade-enter");
+        removeClass(el, "fade-enter-to");
+        removeClass(el, "fade-enter-active");
+      } else {
+        this.show = true;
+        el.style.display = "block";
+        addClass(el, "fade-enter");
+        addClass(el, "fade-enter-active");
+        raf(function() {
+          raf(function() {
+            removeClass(el, "fade-enter");
+            addClass(el, "fade-enter-to");
+            let onend = function() {
+              console.log("执行完毕！");
+              removeClass(el, "fade-enter-to");
+              removeClass(el, "fade-enter-active");
+              el.removeEventListener("transitionend", onend);
+            };
+            el.addEventListener("transitionend", onend);
+          });
+        });
+      }
 
-            // console.log(this);
-        },
-        beforeEnter(el){
-            console.log(el);
-        },
-        enter(el,done){
-            console.log(el);
-             console.log(done);
-            // alert('enter');
-            setTimeout(function(){
-                done();
-            },2000);
-            
-        },
-        afterEnter(el){
-            console.log('完成');
-        },
-        enterCancelled(){
-             console.log('取消');
-        }
+      // console.log(this);
+    },
+    beforeEnter(el) {},
+    enter(el, done) {
+      setTimeout(function() {
+        done();
+      }, 2000);
+    },
+    afterEnter(el) {
+      console.log("完成");
+    },
+    enterCancelled() {
+      console.log("取消");
     }
+  }
 };
 </script>
 
@@ -164,163 +159,168 @@ export default {
 @import "../less/mixins.less";
 
 .bem-dialog {
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 10000;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10000;
 
-    background: #fff;
+  background: #fff;
 }
 
 .bem-dialog__header {
-    text-align: center;
-    position: relative;
+  text-align: center;
+  position: relative;
 }
 
 .bem-dialog__title_border {
-    border-bottom: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
 }
 
 .bem-dialog__title_margin10 {
-    margin-left: 10px;
-    margin-right: 10px;
+  margin-left: 10px;
+  margin-right: 10px;
 }
 
 .bem-dialog__title {
-    display: inline-block;
+  display: inline-block;
 
-    font-size: 16px;
-    color: #333;
+  font-size: 16px;
+  color: #333;
 }
 
 .bem-dialog__closeed {
-    position: absolute;
-    top: 50%;
+  position: absolute;
+  top: 50%;
 
-    width: 14px;
-    height: 14px;
+  width: 14px;
+  height: 14px;
 
-    margin-top: -7px;
+  margin-top: -7px;
 }
 
 .bem-dialog__closeed_right {
-    right: 10px;
+  right: 10px;
 }
 
 .icon-close {
-    width: 100%;
-    height: 100%;
-    text-indent: -100000px;
-    cursor: pointer;
-    outline: 0;
-    overflow: hidden;
-    opacity: 0.6;
-    transition: all 0.5s;
-    background: url(../assets/close.png) no-repeat center;
+  width: 100%;
+  height: 100%;
+  text-indent: -100000px;
+  cursor: pointer;
+  outline: 0;
+  overflow: hidden;
+  opacity: 0.6;
+  transition: all 0.5s;
+  background: url(../assets/close.png) no-repeat center;
 }
 
 .icon-close:hover {
-    opacity: 1;
-    transform: rotate(-180deg);
+  opacity: 1;
+  transform: rotate(-180deg);
 }
 
 .bem-dialog__content {
-    font-size: 14px;
-    padding: 15px;
+  font-size: 14px;
+  padding: 15px;
 }
 
-.bem-dialog__footer {}
+.bem-dialog__footer {
+}
 
 .bem-btn {
-    display: inline-block;
+  display: inline-block;
 
-    border: 0 solid transparent;
-    font-size: 14px;
-    color: #666;
-    background: transparent;
-    border-radius: 3px;
+  border: 0 solid transparent;
+  font-size: 14px;
+  color: #666;
+  background: transparent;
+  border-radius: 3px;
 }
 
 .bem-btn_color-blue_shadow {
-    border: 1px solid #0066ff;
-    color: #0066ff;
-    box-shadow: 5px 5px 5px rgba(0, 102, 255, 0.3);
+  border: 1px solid #0066ff;
+  color: #0066ff;
+  box-shadow: 5px 5px 5px rgba(0, 102, 255, 0.3);
 }
 
 .bem-btn_color-blue_shadow:hover {
-    .transition(all 0.3s);
-    color: #fff;
-    background: rgba(0, 102, 255, 0.7);
+  .transition(all 0.3s);
+  color: #fff;
+  background: rgba(0, 102, 255, 0.7);
 }
 
 .bem-dialog_color-blue_shadow {
-    .bem-btn_color-blue_shadow();
+  .bem-btn_color-blue_shadow();
 }
 
 .payment-info {
-    .mask {
-        position: fixed;
-        top: 0;
-        left: 0;
-        z-index: 9999;
+  .mask {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 9999;
 
-        width: 100%;
-        height: 100%;
+    width: 100%;
+    height: 100%;
 
-        background: rgba(0, 0, 0, 0.5);
-    }
+    background: rgba(0, 0, 0, 0.5);
+  }
 
-    .bem-dialog {
-        left: 50%;
-        top: 50%;
-        min-width: 600px;
-        margin-left: -250px;
-        margin-top: -100px;
-    }
+  .bem-dialog {
+    left: 50%;
+    top: 50%;
+    min-width: 600px;
+    margin-left: -250px;
+    margin-top: -100px;
+  }
 
-    .bem-dialog__header {}
+  .bem-dialog__header {
+  }
 
-    .bem-dialog__title {
-        color: #2a7efd;
-        line-height: 46px;
-    }
+  .bem-dialog__title {
+    color: #2a7efd;
+    line-height: 46px;
+  }
 
-    .bem-dialog__content {
-        padding: 40px 0;
+  .bem-dialog__content {
+    padding: 40px 0;
 
-        color: #000;
-        text-align: center;
+    color: #000;
+    text-align: center;
 
-        box-sizing: border-box;
-    }
+    box-sizing: border-box;
+  }
 
-    .bem-dialog__footer {
-        text-align: center;
-        padding: 0 0 20px 0;
-    }
+  .bem-dialog__footer {
+    text-align: center;
+    padding: 0 0 20px 0;
+  }
 
-    .bem-btn {
-        padding: 5px 10px;
-    }
+  .bem-btn {
+    padding: 5px 10px;
+  }
 }
 
 .fade-enter {
-    opacity: 0;
-    font-size: 45px !important;
+  opacity: 0;
+  font-size: 45px !important;
 }
 
 .fade-enter-active {
-    transition: all 2s;
+  transition: all 2s;
 }
 
 .fade-enter-to {
-    opacity: 1;
-    font-size: 90px !important;
+  opacity: 1;
+  font-size: 90px !important;
 }
 
-.fade-leave {}
+.fade-leave {
+}
 
-.fade-leave-active {}
+.fade-leave-active {
+}
 
-.fade-leave-to {}
+.fade-leave-to {
+}
 </style>
